84fb288dca75f09593441b66b6f6d240eb7814b1
Pierrot31 <plaurens@etud.insa-toulouse.fr>
2017-12-30 11:36:15 +0100

    évolution Be COO/POO: lien à base de données, affichage des valeurs BD sur pages repair 30/12/2017 11:30

---
+++ src/connexion/DataDBB.java
@@ -0,0 +1,24 @@
+package connexion;
+
+public class DataDBB {
+    private final String url, user, passwd;
+
+    public DataDBB() {
+
+        url = "jdbc:mysql://localhost:3306/captainbdd";
+        user = "root";
+        passwd = "xxxxxx";
+
+    }
+    public String useURL(){
+        return url;
+    }
+    public String useUser(){
+        return user;
+    }
+    public String usePasswd(){
+        return passwd;
+    }
+
+
+}
